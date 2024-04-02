e8fba6a5e592aa05587059a4f1857f70f5fc0430
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2024-04-02 11:17:41 -0400

    starting to add javascript to shop page!!!!

---
+++ Frontend/common/shop.js
@@ -0,0 +1,14 @@
+
+const addBike1 = () =>{
+    let input = document.getElementById("bike1");
+    console.log(input.value);
+    
+    
+}
+
+onload = () => {
+
+    let button1 = document.getElementById("button1");
+    button1.onclick = addBike1;
+
+}
