92d880dcb0cec71885e0247394598450efab4722
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2024-04-11 13:48:15 -0400

    starting data validation!!!!

--- Frontend/common/submit.js
+++ Frontend/common/submit.js
@@ -1,7 +1,6 @@
 
 
 const submitForm = () => {
-    console.log("Form done")
 
     validateData();
     cookieData();
@@ -10,6 +9,20 @@ const submitForm = () => {
 
 const validateData = () => {
     
+    const firstName = document.form.firstname.value;
+    const lastName = document.form.lastname.value;
+    const email = document.form.email.value;
+    const sch = document.form.sch.value;
+    const snumber = document.form.snumber.value;
+    const address = document.form.address.value;
+    const city = document.form.city.value;
+    const state = document.form.state.value;
+    const zip = document.form.zip.value;
+
+    RegExpText = /^[A-Z a-z]+$/;
+	RegExpST = /^[A-Za-z]{2}$/;
+    RegExpZip = /^[0-9]{5}$/;
+
 }
 
 
