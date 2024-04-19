18b4608289441529976b6ab9121652608042b64c
John <john.groton@mymail.champlain.edu>
2024-04-19 09:07:07 -0400

    making stuff look pretty (cookies work now)

--- Frontend/php/submit.php
+++ Frontend/php/submit.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html>
-<body>
+
+<head>
+    <link rel="stylesheet" href="../common/phpsubmit.css">
+    <title>New-England-Bikes-Confirm</title>
+    <script type="text/javascript" src="../common/submit.js"></script> 
+</head>
+
+<body class="text body">
+
+<div class="navbar navBarFlex">
+            <div><a href="../htm/home.htm">Home</a></div>
+            <div> <a href="../htm/shop.html">Shop</a></div>
+            <div><a href="../htm/cart.html">Cart</a></div>
+            <div><a href="../htm/about.html">About</a></div>
+</div>
 
 
+<div class="grid-container">
 <?php
-
-var_dump($_COOKIE);
 
 $firstName =  $_COOKIE["firstName"];
 $lastName = $_COOKIE["lastName"];
@@ -16,7 +29,9 @@ $city = $_COOKIE["city"];
 $state = $_COOKIE["state"];
 $zip = $_COOKIE["zip"];
 
-echo "<p>First name: $firstName</p>"; 
+echo '<div class="info rectangle">';
+echo "<p >Order confirmed, here is your info!</p>";
+echo "<p >First name: $firstName</p>"; 
 echo "<p>Last Name: $lastName</p>"; 
 echo "<p>Email: $email</p>"; 
 echo "<p>Phone Number: $phone</p>"; 
@@ -24,9 +39,10 @@ echo "<p>Address: $addr</p>";
 echo "<p>City: $city</p>"; 
 echo "<p>State: $state</p>"; 
 echo "<p>Zip Code: $zip</p>"; 
+echo "</div>"
 
 ?>
-
+</div>
 
 </body>
 </html>
