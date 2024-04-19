d9df8808e2561b94c3ce155d4e1ecf54d63e337f
John <john.groton@mymail.champlain.edu>
2024-04-19 09:35:46 -0400

    got some data into database!!!

--- Frontend/php/submit.php
+++ Frontend/php/submit.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="../common/phpsubmit.css">
     <title>New-England-Bikes-Confirm</title>
     <script type="text/javascript" src="../common/submit.js"></script> 
+    <link rel="icon" type="image/x-icon" href="../img/bike.svg">
 </head>
 
 <body class="text body">
@@ -20,6 +21,8 @@
 <div class="grid-container">
 <?php
 
+
+//getting cookies
 $firstName =  $_COOKIE["firstName"];
 $lastName = $_COOKIE["lastName"];
 $email = $_COOKIE["email"];
@@ -29,6 +32,7 @@ $city = $_COOKIE["city"];
 $state = $_COOKIE["state"];
 $zip = $_COOKIE["zip"];
 
+//outputing cookies
 echo '<div class="info rectangle">';
 echo "<p >Order confirmed, here is your info!</p>";
 echo "<p >First name: $firstName</p>"; 
@@ -39,7 +43,30 @@ echo "<p>Address: $addr</p>";
 echo "<p>City: $city</p>"; 
 echo "<p>State: $state</p>"; 
 echo "<p>Zip Code: $zip</p>"; 
-echo "</div>"
+echo "<p>Thanks for shopping with us!</p>"; 
+echo "</div>";
+
+
+//connect to the db
+$host = "localhost";
+$user = "root";
+$db = "customers";
+$pass = "";
+
+$customersLink = mysqli_connect($host,$user,$pass,$db); 
+if ($customersLink->connect_errno>0) 
+{
+    die('Could not connect: ' . $db->error ); 
+}
+
+$db_selected = mysqli_select_db($customersLink, $db); 
+if (!$db_selected) 
+{
+    die ('Can\'t use database $db : ' . $db->error); 
+}
+
+// Insert some data
+$customersLink -> query("INSERT INTO customer VALUES (1, '$firstName', '$lastName')");
 
 ?>
 </div>
