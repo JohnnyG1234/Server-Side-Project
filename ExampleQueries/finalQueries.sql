c0c4710de06faa77f99398fb2ccd8081437ef72c
John <john.groton@mymail.champlain.edu>
2024-04-30 10:54:52 -0400

    final queries

---
+++ ExampleQueries/finalQueries.sql
@@ -0,0 +1,10 @@
+
+SELECT * FROM customers.address;
+SELECT * FROM customers.customer;
+SELECT * FROM customers.customercontact;
+
+SELECT * FROM neb_orders.lineitem;
+SELECT * FROM neb_orders.orderdata;
+SELECT * FROM neb_orders.orderpaycard;
+
+SELECT * FROM payments.customer_card;
